ade8af28ed03e7f7bdd0cf6356d6b612e7317cd0
Maria <mariakalikas@gmail.com>
2017-09-17 23:05:47 -0400

    converted tabs to spaces

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 int main() {
 
-	// creates (+ 3 4)
-	auto* e = new arith_expr{add_op, new int_lit(3), new int_lit(4)};
+  // creates (+ 3 4)
+  auto* e = new arith_expr{add_op, new int_lit(3), new int_lit(4)};
 
-	// creates (and true false)
-	auto* e2 = new logical_expr{
-		and_op, new bool_lit(true), new bool_lit(false)};
+  // creates (and true false)
+  auto* e2 = new logical_expr{
+    and_op, new bool_lit(true), new bool_lit(false)};
 
-	return 0;
+  return 0;
 
 }
