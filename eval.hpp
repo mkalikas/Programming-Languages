8389c99f8bfa490661abdb12a22a5d753aa77512
Maria <mariakalikas@gmail.com>
2017-09-18 03:36:59 -0400

    added eval.hpp

--- eval.hpp
+++ eval.hpp
@@ -2,10 +2,10 @@
 #include "el_ast.hpp"
 
 // Valuation functions for numeric expressions
-int eval_program(prog* p);
+int eval_program(program* p);
 int eval_num_expr(num_expr* e);
 int eval_int_lit(int_lit* e);
 int eval_arg_expr(arg_expr* e);
 int eval_arith_expr(arith_expr* e);
-int eval_if_expr(if_expr* e);
+
 
